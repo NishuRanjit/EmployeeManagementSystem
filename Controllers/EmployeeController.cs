3f93495ef8d0e823ca0b3b172e2c705a0b82f182
Nishu Ranjit <nishuranjit55@gmail.com>
2025-12-27 01:17:37 +0545

    Add project files.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TaskEmployeeManagementSystem.Models;
+
+namespace TaskEmployeeManagementSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : Controller
+    {
+      
+        private static List<Employee> _staticEmployee=new List<Employee>();
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult EmployeeData(Employee employee)
+        {
+            var empdata = new Employee();
+            empdata.Id = employee.Id;
+            empdata.Name=employee.Name;
+            empdata.Role=employee.Role;
+            empdata.Salary=employee.Salary;
+            empdata.Bonus=employee.Bonus;
+
+
+
+            _staticEmployee.Add(employee);
+       return RedirectToAction("Index"); }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿namespace TaskEmployeeManagementSystem.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Role { get; set; } = "Employee";
+        public decimal Salary{ get; set; }
+        public decimal Bonus{ get; set; }
+
+      
+       
+    }
+}
